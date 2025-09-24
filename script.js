ef407cf1405a67f21ff119598738d2c1bc65577d
unknown <pedrinho.jpln@gmail.com>
2025-09-24 13:07:23 -0300

    validação do CEP criada

--- script.js
+++ script.js
@@ -126,3 +126,52 @@ window.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.produto__detalhes').innerHTML = '<h1>Produto não encontrado!</h1>';
     }
 });
+
+// validação do CEP
+
+const cep = document.querySelector('#cep');
+const rua = document.querySelector('#rua');
+const bairro = document.querySelector('#bairro');
+const cidade = document.querySelector('#cidade');
+const estado = document.querySelector('#estado');
+const message = document.querySelector('#message');
+const enderecoResultado = document.querySelector('#endereco-resultado');
+
+cep.addEventListener('focusout', async() => {
+
+    try {
+        const onlyNumbers = /^[0-9]+$/;
+        const cepValid = /^[0-9]{8}$/;
+
+        if(!onlyNumbers.test(cep.value) || !cepValid.test(cep.value)) {
+            throw { cep_error: 'CEP inválido! Digite apenas os 8 números.'};
+        }
+        const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);     
+
+        if(!response.ok) {
+            throw await response.json();
+        } 
+        const responseCep = await response.json();
+
+        if (responseCep.erro) {
+            throw { cep_error: 'CEP não encontrado!' };
+        }
+
+        message.textContent = '';
+        rua.value = responseCep.logradouro;
+        bairro.value = responseCep.bairro;
+        cidade.value = responseCep.localidade;
+        estado.value = responseCep.uf;
+        enderecoResultado.style.display = 'block';
+
+    } catch (error) {
+        if(error?.cep_error) {
+            message.textContent = error.cep_error;
+            enderecoResultado.style.display = 'none';
+            setTimeout(() => {
+                message.textContent = '';
+            }, 5000);
+        } 
+        console.log(error);
+    }
+});
